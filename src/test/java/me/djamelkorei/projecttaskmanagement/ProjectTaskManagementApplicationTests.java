c3964575e83495a3e1031609c41afec8a616ad9a
djamel-kr <djamel.korei@gmail.com>
2021-12-24 17:49:13 +0100

    initial commit

---
+++ src/test/java/me/djamelkorei/projecttaskmanagement/ProjectTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.djamelkorei.projecttaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
